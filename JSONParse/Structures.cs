160f9266f4b238866fa4e1a87bc85d108952a607
Александр <vk-tester@yandex.ru>
2020-07-29 21:26:17 +0600

    JSON parse

---
+++ JSONParse/Structures.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONParse
+{
+    class Structures
+    {
+        public int Sum { get; set; }
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
