6613ba6b2254c69898efc4ad70ddf178eaf44573
Александр <vk-tester@yandex.ru>
2020-08-22 23:00:03 +0600

    Work POST method

--- WebAPIEvent/Controllers/EventsController.cs
+++ WebAPIEvent/Controllers/EventsController.cs
@@ -27,20 +27,27 @@ namespace WebAPIEvent.Controllers
         //api/events/keys?unitId=1&take=3&skip=3
         [HttpGet]
         [Route("keys")]
-        public async Task<string> GetEventIdsAsync(int unitId, int take, int skip)
+        public async Task<ActionResult> GetEventIdsAsync(int unitId, int take, int skip)
         {
             var dirInfo = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Parent?.Parent?.Parent;
             string path = Path.Combine(dirInfo.FullName, "events.json");
-            List<Event> events = JsonConvert.DeserializeObject<List<Event>>(await System.IO.File.ReadAllTextAsync(path));
-            var filteredEvents = events.Where(u => u.UnitId == unitId).Skip(skip).Take(take);
-            JArray array = new JArray(filteredEvents.Select(s => s.Id).ToArray());
-            return array.ToString();
+            List<JObject> events = JsonConvert.DeserializeObject<List<JObject>>(await System.IO.File.ReadAllTextAsync(path));
+            var filteredEvents = events.Where(u => u.Value<int>("UnitId") == unitId).Skip(skip).Take(take);
+            JArray array = new JArray(filteredEvents.Select(s => s.Value<int>("Id")).ToArray());
+            return Ok(array.ToString());//array.ToString();
         }
 
         // POST api/<EventsController>
         [HttpPost]
-        public void Post([FromBody] int[] array)
+        public async Task<ActionResult<string>> Post([FromBody] int[] ids)
         {
+            var dirInfo = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Parent?.Parent?.Parent;
+            string path = Path.Combine(dirInfo.FullName, "events.json");
+            List<JObject> events = JsonConvert.DeserializeObject<List<JObject>>(System.IO.File.ReadAllText(path));
+            var filteredEvents = events.Where(u => ids.Contains(u.Value<int>("Id")));
+            //JArray array = new JArray(filteredEvents.ToArray());
+            var result = JsonConvert.SerializeObject(filteredEvents);
+            return Ok(result);
         }
 
         // PUT api/<EventsController>/5
