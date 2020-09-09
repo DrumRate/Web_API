4ce33b3a085574f8356128f25df2e8c0219a627f
Александр Пелипенко <vk-tester@yandex.ru>
2020-09-09 13:06:22 +0600

    WPF App

---
+++ WPFApp/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace WPFApp.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public double StorageValue { get; set; }
+        public string Name { get; set; }
+        public int UnitId { get; set; }
+        public string Description { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
